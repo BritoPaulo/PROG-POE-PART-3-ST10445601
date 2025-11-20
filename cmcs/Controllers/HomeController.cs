f2efb1b8933720511b5d0e79ee0781c87a576452
st10445601 <st10445601@imconnect.edu.za>
2025-11-20 15:29:46 +0200

    Added to home controller

--- cmcs/Controllers/HomeController.cs
+++ cmcs/Controllers/HomeController.cs
@@ -94,7 +94,7 @@ namespace cmcs.Controllers
                 .OrderByDescending(c => c.SubmittedDate)
                 .ToList();
 
-            // Add ViewBag values for statistics and debug informatio
+            // Add ViewBag values for statistics and debug informat
             ViewBag.AllClaimsCount = _context.Claims.Count();
             ViewBag.PendingClaimsCount = pendingClaims.Count;
             ViewBag.AllClaims = _context.Claims
